b77ea37b572027be1da608267d64decc2d3a4b22
Maxime Lerasle <maxlerasle@gmail.com>
2024-02-26 16:32:39 +0100

    Sentry env variables

--- next.config.js
+++ next.config.js
@@ -15,4 +15,11 @@ const nextConfig = {
   },
 };
 
-module.exports = withSentryConfig(nextConfig);
+const sentryWebpackPluginOptions = {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  silent: true,
+};
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
